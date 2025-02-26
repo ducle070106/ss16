de32731f3da98eacc163aa3c9de4830d299b537e
ducle070106 <ducle070106@gmail.com>
2025-02-26 22:10:54 +0700

    b2.js

---
+++ b2.js
@@ -0,0 +1,5 @@
+let number=10;
+let price=20.5;
+let status= 10>5;
+let city="Hà Nội";
+document.write("Biến <b>number</b> kiểu số nguyên, có giá trị là <b>",number,"</b><br>Biến <b>price</b> kiểu số thực, có giá trị là <b>",price,"</b><br>Biến <b>status</b> kiểu logic, có giá trị là <b>",status,"</b><br>Biến <b>city</b> kiểu chuỗi, có giá trị là <b>\"",city,"\"</b>");
