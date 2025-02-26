61476fec48a7c47666155a2fd2b978d737e4d2a2
ducle070106 <ducle070106@gmail.com>
2025-02-26 22:18:45 +0700

    b4.js

---
+++ b4.js
@@ -0,0 +1,4 @@
+let width=prompt("Nhập chiều rộng hình chữ nhật");
+let height=prompt("Nhập chiều cao hình chữ nhật");
+let area=width*height;
+document.write("<h1>Diện tích hình chữ nhật có chiều dài ",width," và chiều rộng ",height," là ",area,"</h1>");
