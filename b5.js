9ad9ca0a0816f01beffd93a6ff80e3946834f11c
ducle070106 <ducle070106@gmail.com>
2025-02-26 22:22:02 +0700

    b5.js

---
+++ b5.js
@@ -0,0 +1,6 @@
+var firstNumber = prompt("Nhập số thứ nhất:");
+var secondNumber = prompt("Nhập số thứ hai:");
+firstNumber = parseFloat(firstNumber);
+secondNumber = parseFloat(secondNumber);
+var sum = firstNumber + secondNumber;
+alert("Tổng hai số là: " + sum);
