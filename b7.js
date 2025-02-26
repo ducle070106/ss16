54e826308bd0fb4442d246a168388bc328942b59
ducle070106 <ducle070106@gmail.com>
2025-02-26 22:27:29 +0700

    b7.js

---
+++ b7.js
@@ -0,0 +1,4 @@
+let math=+prompt("Nhập điểm môn toán");
+let physics=+prompt("Nhập điểm môn vật lý");
+let chemistry=+prompt("Nhập điểm môn hóa");
+document.write("<h1>điểm trung bình 3 môn bằng ",(math + physics + chemistry ) / 3,"</h1>");
